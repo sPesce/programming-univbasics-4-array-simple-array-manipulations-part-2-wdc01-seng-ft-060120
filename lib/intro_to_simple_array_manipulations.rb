23f801898713ccf779d8811221f2a7b238fa2b42
stephen pesce <stevepesce879@gmail.com>
2020-04-17 20:56:45 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, ele)
+  array.insert(4,ele)
+end
+
+def using_uniq(array)
+  array.uniq 
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, str)
+  array.delete(str)
+end
+
+def using_delete_at(array, i)
+  array.delete_at(i)
+end
